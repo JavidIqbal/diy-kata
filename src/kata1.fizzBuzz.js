a69963e0dff5c413b6ca0b70d7af3ebb84dec5a9
JavidIqbal <jav99@hotmail.com>
2018-02-24 18:54:09 +0000

    diy kata 1 of 4 complete

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,12 @@
-const fizzBuzz = (number) => {
+export const fizzBuzz = (number) => {
 
-}
-
-export default fizzBuzz
+    if (i % 3 === 0 && i % 5 !== 0) {
+        return ("Fizz");
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+        return ("Buzz");
+    } else if (i % 5 === 0 && !i % 3 === 0) {
+        return ("FizzBuzz");
+    } else {
+        return (i);
+    };
+};
